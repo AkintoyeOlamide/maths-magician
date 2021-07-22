e7b74a6a1cf29e56febc1813246d9e50c232785c
olamidelanky <akintoyeolamide95@gmail.com>
2021-07-22 20:40:17 +0100

    finished working on project

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,11 +6,30 @@ const Home = () => (
   <div>
     <h2 className={homeStyles.heading}>What is math!</h2>
     <div>
-      <p className={homeStyles.paragraph}>Mathematicians seek and use patterns[8][9] to formulate new conjectures; they resolve the truth or falsity of such by mathematical proof. When mathematical structures are good models of real phenomena, mathematical reasoning can be used to provide insight or predictions about nature. Through the use of abstraction and logic, mathematics developed from counting, calculation, measurement, and the systematic study of the shapes and motions of physical objects. Practical mathematics has been a human activity from as far back as written records exist. The research required to solve mathematical problems can take years or even centuries of sustained inquiry.</p>
+      <p className={homeStyles.paragraph}>
+        mathematics, the science of structure, order, and relation that has
+        evolved from elemental practices of counting, measuring, and describing
+        the shapes of objects. It deals with logical reasoning and quantitative
+        calculation, and its development has involved an increasing degree of
+        idealization and abstraction of its subject matter.
+        {' '}
+      </p>
 
-      <p className={homeStyles.paragraph}>Rigorous arguments first appeared in Greek mathematics, most notably in Euclids Elements. Since the pioneering work of Giuseppe Peano (1858–1932), David Hilbert (1862–1943), and others on axiomatic systems in the late 19th century, it has become customary to view mathematical research as establishing truth by rigorous deduction from appropriately chosen axioms and definitions. Mathematics developed at a relatively slow pace until the Renaissance, when mathematical innovations interacting with new scientific discoveries led to a rapid increase in the rate of mathematical discovery that has continued to the present day.</p>
+      <p className={homeStyles.paragraph}>
+        {' '}
+        Since the 17th century, mathematics has been an indispensable adjunct to
+        the physical sciences and technology, and in more recent times it has
+        assumed a similar role in the quantitative aspects of the life sciences.
+      </p>
 
-      <p className={homeStyles.paragraph}>Mathematics is essential in many fields, including natural science, engineering, medicine, finance, and the social sciences. Applied mathematics has led to entirely new mathematical disciplines, such as statistics and game theory. Mathematicians engage in pure mathematics (mathematics for its own sake) without having any application in mind, but practical applications for what began as pure mathematics are often discovered later.</p>
+      <p className={homeStyles.paragraph}>
+        In many cultures—under the stimulus of the needs of practical pursuits,
+        such as commerce and agriculture—mathematics has developed far beyond
+        basic counting. This growth has been greatest in societies complex
+        enough to sustain these activities and to provide leisure for
+        contemplation and the opportunity to build on the achievements of
+        earlier mathematicians.
+      </p>
     </div>
   </div>
 );
